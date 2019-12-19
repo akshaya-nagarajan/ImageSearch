2d558a8547461de0f9fd47bd9eea5fd08baf4a49
Akshaya Nagarajan <57167636+akshaya-nagarajan@users.noreply.github.com>
2019-12-18 20:41:39 -0800

    Add files via upload

---
+++ app/image-list/image-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../shared/image.service';
+
+@Component({
+  selector: 'app-image-list',
+  templateUrl: './image-list.component.html',
+  styleUrls: ['./image-list.component.css']
+})
+export class ImageListComponent implements OnInit {
+  images: any[];
+
+  constructor(private _imageService: ImageService) { }
+
+  handleSuccess(data) {
+     this.images = data.hits;
+  }
+  searchImages(query: string) {
+  	return this._imageService.getImage(query).subscribe(
+  		data => this.handleSuccess(data),
+  		error => console.log(error),
+  		() => console.log("Request complete")
+	  )
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ environments/environment.ts
@@ -0,0 +1,10 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  PIXABAY_API_KEY: '8759249-040c8f55811c12d66021cadf9',
+  PIXABAY_API_URL: 'https://pixabay.com/api/?key='
+};
